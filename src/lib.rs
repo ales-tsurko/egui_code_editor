65ce5757f821629f09c19ddf2d3ae5c8872da090
Ales Tsurko <ales.tsurko@gmail.com>
2026-01-09 23:07:18 +0100

    Actualize api

--- src/lib.rs
+++ src/lib.rs
@@ -350,7 +350,7 @@ impl CodeEditor {
                     self.numlines_show(h, text.as_str());
                 }
                 egui::ScrollArea::horizontal()
-                    .id_source(format!("{}_inner_scroll", self.id))
+                    .id_salt(format!("{}_inner_scroll", self.id))
                     .show(h, |ui| {
                         let mut layouter =
                             |ui: &egui::Ui, text_buffer: &dyn TextBuffer, _wrap_width: f32| {
@@ -371,7 +371,7 @@ impl CodeEditor {
         };
         if self.vscroll {
             egui::ScrollArea::vertical()
-                .id_source(format!("{}_outer_scroll", self.id))
+                .id_salt(format!("{}_outer_scroll", self.id))
                 .stick_to_bottom(self.stick_to_bottom)
                 .show(ui, code_editor);
         } else {
